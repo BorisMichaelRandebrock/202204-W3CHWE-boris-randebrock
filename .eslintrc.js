e1b700e17927ca57caa1448852fbbac43f3e026a
Boris <boris@randebrock.com>
2022-04-22 19:24:26 +0200

    initial setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,20 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
+    jest: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ["airbnb-base", "prettier;"],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "lines-between-class-members": [
+      "error",
+      "always",
+      { exceptAfterSingleLine: true },
+    ],
   },
 };
